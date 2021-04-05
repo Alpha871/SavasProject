82f7b27da6843d4c7be08e758975be53c1d4a63d
Alpha <Bahalphaabdoulaye40@gmail.com>
2021-04-04 22:55:26 -0700

    Ajoutez des fichiers projet.

---
+++ Savas.Destop/Form.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Savas.Destop
+{
+    partial class AnaForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaForm));
+            this.bilgiPanel = new System.Windows.Forms.Panel();
+            this.ucaksavarPanel = new System.Windows.Forms.Panel();
+            this.savasAlaniPanel = new System.Windows.Forms.Panel();
+            this.bilgiLabel = new System.Windows.Forms.Label();
+            this.sureLabel = new System.Windows.Forms.Label();
+            this.bilgiPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bilgiPanel
+            // 
+            this.bilgiPanel.BackColor = System.Drawing.Color.SteelBlue;
+            this.bilgiPanel.Controls.Add(this.sureLabel);
+            this.bilgiPanel.Controls.Add(this.bilgiLabel);
+            this.bilgiPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bilgiPanel.Location = new System.Drawing.Point(0, 0);
+            this.bilgiPanel.Name = "bilgiPanel";
+            this.bilgiPanel.Size = new System.Drawing.Size(1244, 121);
+            this.bilgiPanel.TabIndex = 0;
+            // 
+            // ucaksavarPanel
+            // 
+            this.ucaksavarPanel.BackColor = System.Drawing.Color.Teal;
+            this.ucaksavarPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ucaksavarPanel.Location = new System.Drawing.Point(0, 497);
+            this.ucaksavarPanel.Name = "ucaksavarPanel";
+            this.ucaksavarPanel.Size = new System.Drawing.Size(1244, 50);
+            this.ucaksavarPanel.TabIndex = 1;
+            // 
+            // savasAlaniPanel
+            // 
+            this.savasAlaniPanel.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.savasAlaniPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.savasAlaniPanel.Location = new System.Drawing.Point(0, 121);
+            this.savasAlaniPanel.Name = "savasAlaniPanel";
+            this.savasAlaniPanel.Size = new System.Drawing.Size(1244, 376);
+            this.savasAlaniPanel.TabIndex = 2;
+            // 
+            // bilgiLabel
+            // 
+            this.bilgiLabel.AutoSize = true;
+            this.bilgiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.bilgiLabel.Location = new System.Drawing.Point(12, 20);
+            this.bilgiLabel.Name = "bilgiLabel";
+            this.bilgiLabel.Size = new System.Drawing.Size(611, 78);
+            this.bilgiLabel.TabIndex = 0;
+            this.bilgiLabel.Text = "Oyunu başlatmak için ENTER tuşuna basın.\r\nUçaksavarı hareket ettirmek için SAĞ/SO" +
+    "Lyön tuşlarına basın.\r\nAteş etmek için BOŞLUK tuşuna basın.,\r\n";
+            // 
+            // sureLabel
+            // 
+            this.sureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.sureLabel.Location = new System.Drawing.Point(1034, 20);
+            this.sureLabel.Name = "sureLabel";
+            this.sureLabel.Size = new System.Drawing.Size(185, 64);
+            this.sureLabel.TabIndex = 1;
+            this.sureLabel.Text = "0:00";
+            this.sureLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // AnaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 547);
+            this.Controls.Add(this.savasAlaniPanel);
+            this.Controls.Add(this.ucaksavarPanel);
+            this.Controls.Add(this.bilgiPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AnaForm";
+            this.Text = "Savaş";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.AnaForm_Load);
+            this.bilgiPanel.ResumeLayout(false);
+            this.bilgiPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel bilgiPanel;
+        private System.Windows.Forms.Label sureLabel;
+        private System.Windows.Forms.Label bilgiLabel;
+        private System.Windows.Forms.Panel ucaksavarPanel;
+        private System.Windows.Forms.Panel savasAlaniPanel;
+    }
+}
+
